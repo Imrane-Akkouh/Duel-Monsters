d32a24be42616499a1e09cbee77cd52be65e08d0
Imrane-Akkouh <akkouh.imrane@gmail.com>
2020-08-12 10:49:06 +0100

    Updates

---
+++ precache-manifest.8f9ab482f332f842683f4435a5cab4c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c37457589a79fe34539b39753d14db21",
+    "url": "/duel-monsters/index.html"
+  },
+  {
+    "revision": "a09242bc88cb7755f447",
+    "url": "/duel-monsters/static/css/main.24c2c93a.chunk.css"
+  },
+  {
+    "revision": "9fe45477af7a2be973e1",
+    "url": "/duel-monsters/static/js/2.516f0e5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/duel-monsters/static/js/2.516f0e5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a09242bc88cb7755f447",
+    "url": "/duel-monsters/static/js/main.689a2a86.chunk.js"
+  },
+  {
+    "revision": "c2a65e74190df1486352",
+    "url": "/duel-monsters/static/js/runtime-main.0b2658d4.js"
+  }
+]);
